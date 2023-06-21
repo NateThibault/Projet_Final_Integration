0a0220695b79e38c872b477ee98bbcc9feccc398
pri-carvalho <124946794+pri-carvalho@users.noreply.github.com>
2023-06-21 11:49:13 -0400

    mise à jour MyFooter

--- src/app/components/molecules/myFooter/myFooter.tsx
+++ src/app/components/molecules/myFooter/myFooter.tsx
@@ -1,39 +1,21 @@
+"use client"
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-
 function MyFooter() {
   return (
     <footer>
       <AppBar position="static" sx={{ height: '60px' }}>
-        <Toolbar>
-          <Typography variant="body1" component="div" sx={{ flexWrap: 'wrap' }}>
-          Toufik Dellys |
-          </Typography>
-          <Typography component="div" sx={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>
-            &bull;
-          </Typography>
-          <Typography variant="body1" component="div">
-          Nathan Thibault |
-          </Typography>
-          <Typography component="div" sx={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>
-            &bull;
-          </Typography>
-          <Typography variant="body1" component="div">
-          Priscila Carvalho  | 
-          </Typography>
-          <Typography component="div" sx={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>
-            &bull;
-          </Typography>
-          <Typography variant="body1" component="div" sx={{ marginRight: '1rem' }}>
-          Marie-Pier Dubois | 
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Typography variant="body2" component="div">
-          William Bitton
+        <Toolbar sx={{ justifyContent: 'center' }}>
+          <Typography variant="body1" component="div" sx={{ display: 'flex' }}>
+            <Box sx={{ mr: '0.5rem' }}>Toufik Dellys |</Box>
+            <Box sx={{ mr: '0.5rem' }}>Nathan Thibault |</Box>
+            <Box sx={{ mr: '0.5rem' }}>Priscila Carvalho |</Box>
+            <Box sx={{ mr: '0.5rem' }}>Marie-Pier Dubois |</Box>
+            <Box>William Bitton</Box>
           </Typography>
         </Toolbar>
       </AppBar>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import styles from './page.module.css'
 
 export default function Home() {
