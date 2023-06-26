0c5f65f6eece1ae19b4d73ef8e1d35dbd4c4aaab
WilliamBitton <118488828+WilliamBitton@users.noreply.github.com>
2023-06-26 14:13:10 -0400

    test post

--- src/API/api.tsx
+++ src/API/api.tsx
@@ -1,7 +1,7 @@
 import { ProductForm } from "@/interface/interface"
 
 export async function getProductData(id: string) {
-  const res = await fetch(`https://api-final-qxme.onrender.com/products/${id}`, { method: "GET"})
+  const res = await fetch(`https://api-final-qxme.onrender.com/products/${id}`, { method: "GET" })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
@@ -9,15 +9,17 @@ export async function getProductData(id: string) {
 }
 
 export async function putProductData(id: string, formData: ProductForm) {
-  const res = await fetch(`https://api-final-qxme.onrender.com/products/${id}`, { method: "PUT", body: JSON.stringify(formData)})
+  const res = await fetch(`https://api-final-qxme.onrender.com/products/${id}`, { method: "PUT", body: JSON.stringify(formData) })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
   return res.json()
 }
 
-export async function postProductData(formData: ProductForm) {
-  const res = await fetch(`https://api-final-qxme.onrender.com/products`, { method: "POST", body: JSON.stringify(formData)})
+export async function postProductData(formData: any) {
+  console.log(formData)
+  console.log(JSON.stringify(formData))
+  const res = await fetch(`https://api-final-qxme.onrender.com/products`, { method: "POST", body: formData })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
@@ -25,7 +27,7 @@ export async function postProductData(formData: ProductForm) {
 }
 
 export async function getCategoriesData() {
-  const res = await fetch(`https://api-final-qxme.onrender.com/categories`, { method: "GET"})
+  const res = await fetch(`https://api-final-qxme.onrender.com/categories`, { method: "GET" })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
