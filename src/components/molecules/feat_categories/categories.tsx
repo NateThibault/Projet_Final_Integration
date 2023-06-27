b6b7d2dd884f59bc8e838fdcce2b9bb6e08f5671
Nathan Thibault <116368641+NateThibault@users.noreply.github.com>
2023-06-27 14:46:22 -0400

    Update categories.tsx

--- src/components/molecules/feat_categories/categories.tsx
+++ src/components/molecules/feat_categories/categories.tsx
@@ -115,7 +115,7 @@ const CategoryGrid = () => {
   ];
 
   return (
-    <Box sx={{ height: 'auto', maxHeight: '100%',  width: '80%'}}>
+    <Box sx={{ height: 'auto', maxHeight: '100%',  width: '100%'}}>
       {loading ? (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
           <CircularProgress />
