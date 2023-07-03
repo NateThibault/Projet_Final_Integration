6f2249304c6fe183712648c8293ea9dee6b2c654
TOUFIK DELLYS <dellystofik9@gmail.com>
2023-07-03 15:08:38 -0400

    quelques reglage dans la page categorie

--- src/components/organisms/category-grid/categories-grid.tsx
+++ src/components/organisms/category-grid/categories-grid.tsx
@@ -98,7 +98,7 @@ const CategoryGrid = () => {
           onClick={() => handleModifyButtonClick(params)}
           className={styles.buttonGrid}
         >
-          {t("categories-grid.add")}
+         
           <EditIcon style={{ color: '#2196F3' }} />
         </Button>
       ),
